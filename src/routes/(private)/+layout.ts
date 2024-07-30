89051131ed69150f65537db23380d95a8aff9064
Vivek Shukla <viv3kshukla@gmail.com>
2024-07-30 16:00:21 +0530

    handle empty token value

--- src/routes/(private)/+layout.ts
+++ src/routes/(private)/+layout.ts
@@ -1,3 +1,4 @@
+import { goto } from '$app/navigation';
 import { clearUserToken } from '$lib/auth';
 import type { LayoutLoad } from './$types';
 
@@ -5,24 +6,28 @@ export const ssr = false;
 
 export const load: LayoutLoad = async ({ fetch }) => {
 	const token = localStorage.getItem('token');
-	fetch('/api/user', {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${token}`
-		}
-	})
-		.then((response) => {
-			if (!response.ok) {
-				clearUserToken();
+	if (!token) {
+		goto('/login');
+	} else {
+		fetch('/api/user', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
 			}
-			return response.json();
 		})
-		.then((data) => {
-			console.log(data);
-		})
-		.catch((error) => {
-			console.error(error);
-		});
+			.then((response) => {
+				if (!response.ok) {
+					clearUserToken();
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
 	return {};
 };
